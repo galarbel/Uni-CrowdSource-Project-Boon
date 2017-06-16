a90c1282aa0b0a5b159936eccc2d13968c77dbf7
Ariel Goldberger <argold82@gmail.com>
2017-06-16 22:01:35 +0300

    no message

--- Server/WS/get_user_watchlists.php
+++ Server/WS/get_user_watchlists.php
@@ -2,9 +2,6 @@
 
 include_once '../Global/config.php';
 
-// get POST body
-$post_body = json_decode(file_get_contents('php://input'), true);
-
 // check params
 if (!isset($_POST["username"]) ) {
     badRequest("missing 'username' parameter");
@@ -12,17 +9,9 @@ if (!isset($_POST["username"]) ) {
 if (!isset($_POST["password"]) ) {
     badRequest("missing 'password' parameter");
 }
-if (!isset($_POST["email"]) ) {
-    badRequest("missing 'email' parameter");
-}
-if (!isset($_POST["phone"]) ) {
-    badRequest("missing 'phone' parameter");
-}
 
 $username = $_POST["username"];
 $password = $_POST["password"];
-$email    = $_POST["email"];
-$phone    = $_POST["phone"];
 
 //get user_id from given username
 $getUserIdQuery = "call get_user_id (?)";
@@ -30,11 +19,10 @@ $userIdRaw = $db->rawQuery($getUserIdQuery,[$username])[0];
 $user_id = getNumericParamOrDefault($userIdRaw, "id", true, null);
 
 $db = new MysqliDb ($DBServer, $DBUsername, $DBPassword, $DBName);
-$sqlQuery = "call update_user_details (?,?,?)";
-$db->rawQuery($sqlQuery,[$user_id,$email,$phone]);
-
-$results["data"]["submitSuccess"] = true;
+$sqlQuery = "call get_user_wish_lists (?)";
+$results["data"] = $db->rawQuery($sqlQuery,[$user_id]);
 $results["code"] = 200;
+
 header('Content-type: application/json');
 echo json_encode($results);
 
